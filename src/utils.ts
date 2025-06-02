4e7c59455db73cf16aac8783620039042302eb4e
phev8 <phev8@users.noreply.github.com>
2025-06-02 14:37:57 +0200

    remove unused printSurveyItem function from utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { SurveyItem, isSurveyGroupItem, LocalizedString, SurveyGroupItem, SurveySingleItem, SurveySingleItemResponse } from "./data_types";
+import { SurveyItem, isSurveyGroupItem, SurveyGroupItem, SurveySingleItem, SurveySingleItemResponse } from "./data_types";
 
 export const pickRandomListItem = (items: Array<any>): any => {
   return items[Math.floor(Math.random() * items.length)];
@@ -15,21 +15,6 @@ export const printResponses = (responses: SurveySingleItemResponse[], prefix: st
   }));
 }
 
-export const printSurveyItem = (surveyItem: SurveyItem, prefix: string) => {
-  console.log(prefix + surveyItem.key);
-  if (isSurveyGroupItem(surveyItem)) {
-    surveyItem.items.forEach(i => {
-      printSurveyItem(i, prefix + '\t');
-    })
-  } else {
-    if (!surveyItem.components) { return; }
-    console.log(surveyItem.components.items.map(c => {
-      const content = c.content ? c.content[0] : { parts: [] };
-      return prefix + (content as LocalizedString).parts.join('');
-
-    }).join('\n'));
-  }
-}
 
 export const flattenSurveyItemTree = (itemTree: SurveyGroupItem): SurveySingleItem[] => {
   const flatTree = new Array<SurveySingleItem>();
